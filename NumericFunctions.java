0e87ee5229f628e3a0b777e660d614701554ec1e
CeraStroh <cera.stroh@outlook.com>
2023-12-06 19:52:13 -0600

    add gcd() method
    -greatest common denominator

--- NumericFunctions.java
+++ NumericFunctions.java
@@ -15,6 +15,16 @@ public class NumericFunctions {
       return result;
    }
    
+   public static int gcd(int m, int n) {
+      int prev = 0;
+      while (n > 0) {
+         prev = n;
+         n = m % n;
+         m = prev;
+      }
+      return m;
+   }
+   
    public static void main(String[] args) {
       /* for (int n=1; n<=30; n++) {
          System.out.print(n);
@@ -22,12 +32,16 @@ public class NumericFunctions {
          System.out.println(factorial(n));
       }
       System.out.println(factorial(0));
-      System.out.println(factorial(3)); */
+      System.out.println(factorial(3));
       for (int i=1; i<=9; i++) {
          for (int j=1; j<=9; j++) {
             System.out.println(i + "^" + j + " = " + pow(i, j));
          }
       }
-      // System.out.println(pow(4, 3));
+      for (int i=2; i<=10; i++) {
+         for (int j=2; j<=10; j++) {
+            System.out.println(i + "%" + j + " = " + gcd(i, j));
+         }
+      } */
    }
 }
