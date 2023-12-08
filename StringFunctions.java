1a71154c902b42f015b8bac3de22da7fc933d7df
CeraStroh <cera.stroh@outlook.com>
2023-12-07 19:55:09 -0600

    add count2() & countIgnoreCase()

--- StringFunctions.java
+++ StringFunctions.java
@@ -9,7 +9,34 @@ public class StringFunctions {
       return count;
    }
    
+   public static int count2(String s, String target) {
+      int count = 0;
+      int n = s.length();
+      int i = 0;
+      int index = 0;
+      while (i < n) {
+         index = s.indexOf(target, i);
+         if (index >= 0) {
+            count++;
+         } else break;
+         i = index + target.length();
+      }
+      return count;
+   }
+   
+   public static int countIgnoreCase(String s, String target) {
+      int count = 0;
+      int n = target.length();
+      for (int i=0; i<=s.length()-n; i++) {
+         String piece = s.substring(i, i+n);
+         if (piece.equalsIgnoreCase(target)) count++;
+      }
+      return count;
+   }
+   
    public static void main(String[] args) {
-      System.out.println("Number of this's: " + count("this and this and that and this", "this"));
+      //System.out.println("Number of this's: " + count("This and This and that and this", "this"));
+      //System.out.println("Number of this's: " + countIgnoreCase("This and This and that and this", "this"));
+      System.out.println("Number of this's: " + count2("this and This and that and this", "this"));
    }
 }
