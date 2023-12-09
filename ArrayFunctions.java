da493e3ea0ab6a20bee9ffeb20d13c2774c0117e
CeraStroh <cera.stroh@outlook.com>
2023-12-09 08:22:41 -0600

    add sum(), max(), min(), and display() methods

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -6,9 +6,48 @@ public class ArrayFunctions {
       return -1;
    }
    
+   public static int sum(int[] data) {
+      int total = 0;
+      for (int d : data) {
+         total += d;
+      }
+      return total;
+   }
+   
+   public static int max(int[] data) {
+      int max = 0;
+      for (int d : data) {
+         if (d > max) max = d;
+      }
+      return max;
+   }
+   
+   public static int min(int[] data) {
+      int min = max(data);
+      for (int d : data) {
+         if (d < min) min = d;
+      }
+      return min;
+   }
+   
+   public static void display(int[] data) {
+      for (int d : data) {
+         System.out.print(d + " ");
+      }
+      System.out.println("");
+   }
+   
    public static void main(String[] args) {
       int[] data = {3, 14, 7, 22, 45, 12, 19, 42, 6};
-      System.out.println("Search for 7: " + linearSearch(data, 7));
+      /*System.out.println("Search for 7: " + linearSearch(data, 7));
       System.out.println("Search for 100: " + linearSearch(data, 100));
+      for (int d : data) {
+         System.out.println("Search for " + d + ": " + linearSearch(data, d));
+      }
+      System.out.println("Total sum of data: " + sum(data));
+      System.out.println("Max of data: " + max(data));
+      System.out.println("Min of data: " + min(data));*/
+      display(data);
+      System.out.print("hi");
    }
 }
